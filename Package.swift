210d228c58ca88661d879fdac157dca5f0d49420
Petr Palata <petr.palata@gmail.com>
2021-07-09 18:21:56 +0200

    Include linker/swift settings for imagemagick in the target configuration

--- Package.swift
+++ Package.swift
@@ -4,5 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftImageMagick"
+    name: "SwiftImageMagick",
+    targets: [
+//        .target(
+//            name: "SwiftImageMagick",
+//            swiftSettings: [
+//                .unsafeFlags(["-I/usr/local/Cellar/imagemagick@6/6.9.12-17/include/ImageMagick-6"]),
+//            ],
+//            linkerSettings: [
+//                .unsafeFlags(["-L/usr/local/Cellar/imagemagick@6/6.9.12-17/lib"]),
+//                .linkedLibrary("MagickWand-6.Q16"),
+//                .linkedLibrary("MagickCore-6.Q16"),
+//            ]
+//        ),
+        .systemLibrary(
+            name: "SwiftImageMagick",
+            pkgConfig: "ImageMagick"
+        )
+    ]
 )
