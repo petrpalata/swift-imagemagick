4d01e54a9fefce40b9bd4cd176496526837a4958
Petr Palata <petr.palata@gmail.com>
2021-07-09 18:57:52 +0200

    Setup the ImageMagick wrapper according to a tutorial

---
+++ Headers/SwiftImageMagick-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  SwiftImageMagick-Bridging-Header.h
+//  
+//
+//  Created by Petr Palata on 09.07.2021.
+//
+
+#include <MagickWand/MagickWand.h>
+#include <MagickCore/MagickCore.h>
--- Package.swift
+++ Package.swift
@@ -4,22 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftImageMagick",
-    targets: [
-//        .target(
-//            name: "SwiftImageMagick",
-//            swiftSettings: [
-//                .unsafeFlags(["-I/usr/local/Cellar/imagemagick@6/6.9.12-17/include/ImageMagick-6"]),
-//            ],
-//            linkerSettings: [
-//                .unsafeFlags(["-L/usr/local/Cellar/imagemagick@6/6.9.12-17/lib"]),
-//                .linkedLibrary("MagickWand-6.Q16"),
-//                .linkedLibrary("MagickCore-6.Q16"),
-//            ]
-//        ),
-        .systemLibrary(
-            name: "SwiftImageMagick",
-            pkgConfig: "ImageMagick"
-        )
-    ]
+  name: "SwiftImageMagick",
+  products: [
+    .library(name: "SwiftImageMagick", targets: ["SwiftImageMagick"]),
+  ],
+  targets: [
+    .systemLibrary(name: "SwiftImageMagick"),
+  ]
 )
